81caa7491a7f298f43a77c8950a6003cb64df511
Lashyn <Lashynaskjfsad>
2025-05-11 23:13:45 +0500

    Python

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host='redis', port=6379, db=0)
+r = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
